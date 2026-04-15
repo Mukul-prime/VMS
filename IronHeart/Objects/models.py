5831c7097e7ce14d7b3efc2ae8c4731ebbb06357
CodeByMukul-Tech <mukulvats200@gmail.com>
2026-04-15 16:57:14 +0530

    Updated the files

--- IronHeart/Objects/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ObjectsData(models.Model):
-    Id = models.AutoField(primary_key=True)
-    Name = models.TextField()
-
-    class Meta:
-        db_table = "Objects"
