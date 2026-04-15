8befb45cbf1d7dd84cf1f90f50b418476510438c
Monu Yadav <Mukul-prime@users.noreply.github.com>
2026-04-15 16:17:08 +0530

    final commit with correct email

--- IronHeart/settings.py
+++ IronHeart/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'VMSD',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Monu@12345',
         'HOST': 'localhost',
         'PORT': '3306',
     }
